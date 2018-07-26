d8b205285716d6873257d26a3b8425f51489a27f
Josia50 <38124619+Josia50@users.noreply.github.com>
2018-07-26 14:47:53 +0200

    Fix

--- src/welcome.js
+++ src/welcome.js
@@ -7,20 +7,16 @@ client.on('ready', () => {
     console.log('Welcome/Leave Message enabled!')
 })
 
-
-
 client.on("guildMemberAdd", (member) =>  {
   const guild = member.guild.
   member.guild.channels.find("name", "general").send(`${member.user}, Welcome to the server!`); 
   console.log(`${member.user} Joined`);
 });
 
-  client.on("guildMemberRemove", (member) => {
-    const membertag = member.user.tag
-    member.guild.channels.find("name", "general").send(`${membertag} has left the server!`);
-    console.log(`${member.user} left `);
-  });
+ client.on("guildMemberRemove", (member) => {
+   const membertag = member.user.tag
+   member.guild.channels.find("name", "general").send(`${membertag} has left the server!`);
+   console.log(`${member.user} left `);
+ });
 
-
-  client.login(config.token);
-               
+client.login(config.token);         
